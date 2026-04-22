f0466c6a79a365598ca0d88d0f55c77b16456b63
Anuj06510651 <s24cseu1819@bennett.edu.in>
2026-04-22 13:11:40 +0530

    fix: vercel css loading issue

--- server/store/prototypeStore.js
+++ server/store/prototypeStore.js
@@ -1,6 +1,17 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+// In-memory store (temporary DB)
+const prototypes = new Map();
+
+// Generate unique prototype ID
+export const generatePrototypeId = () => {
+  return 'proto_' + Math.random().toString(36).substr(2, 9);
+};
+
+// Save onboarding data
+export const savePrototypeOnboarding = (id, data) => {
+  prototypes.set(id, data);
+};
+
+// Get onboarding data by ID
+export const getPrototypeOnboardingById = (id) => {
+  return prototypes.get(id);
 };
